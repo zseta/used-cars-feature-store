0f8817e761531df123384a7832b5a6205db7745a
Attila Toth <hello@attilatoth.dev>
2025-05-14 02:29:16 +0200

    fix dc

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ SCYLLA_HOSTS = ["172.18.0.4"]
 SCYLLA_KEYSPACE = 'feature_store'
 SCYLLA_USER = 'scylla'
 SCYLLA_PASS = ''
-SCYLLA_DC = 'dc1'
+SCYLLA_DC = 'datacenter1'
 
 SCHEMA_FILE = 'schema.cql'
 TABLE = 'car_features'
